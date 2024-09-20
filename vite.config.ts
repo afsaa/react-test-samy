e3c8c8a5b4a515dd96a46a72e63f19a32a925bf1
Andres Saa <afsaa@utp.edu.co>
2024-09-20 13:43:44 -0500

    Add path alias to config

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import react from '@vitejs/plugin-react';
+import path from 'path';
 import tailwindcss from 'tailwindcss';
 import { defineConfig } from 'vite';
 
@@ -7,6 +8,11 @@ export default defineConfig({
   css: {
     postcss: {
       plugins: [tailwindcss],
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
