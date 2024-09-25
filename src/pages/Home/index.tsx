eb0c5e4f2369285cede3878ba9ef92fbd14f7c0b
Andres Saa <afsaa@utp.edu.co>
2024-09-25 18:37:05 -0500

    Refactor Home component to update grid layout

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -45,21 +45,21 @@ const Home = (): JSX.Element => {
   return (
     <section className="w-full h-auto bg-gray-200/50">
       {query.length > 0 ? (
-        <div className="w-full grid grid-cols-1 md:grid-cols-2 p-10 gap-10">
+        <div className="w-full grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 p-10 gap-10">
           {filteredProducts.length > 0 ? (
             filteredProducts.map((product) => <ProductCard key={product.id} {...product} />)
           ) : (
-            <h4 className="w-full md:col-span-2 text-3xl text-center">No products found</h4>
+            <h4 className="w-full md:col-span-2 xl:col-span-4 text-3xl text-center">No products found</h4>
           )}
         </div>
       ) : (
         <InfiniteScroll
-          className="w-full grid grid-cols-1 md:grid-cols-2 p-10 gap-10"
+          className="w-full grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 p-10 gap-10"
           dataLength={productsToShow.length}
           next={getNextPage}
           hasMore={hasMore}
-          loader={<h4 className="w-full md:col-span-2 text-3xl text-center">Loading...</h4>}
-          endMessage={<h4 className="w-full md:col-span-2 text-3xl text-center">No more products to show</h4>}
+          loader={<h4 className="w-full md:col-span-2 xl:col-span-4 text-3xl text-center">Loading...</h4>}
+          endMessage={<h4 className="w-full md:col-span-2 xl:col-span-4 text-3xl text-center">No more products to show</h4>}
         >
           {productsToShow.map((product) => (
             <ProductCard key={product.id} {...product} />
